74a4f67fbe4852050357a6422614036b8385177d
Liam Pattinson <liampattinson@gmail.com>
2022-12-09 15:03:58 +0000

    Cmake: Prevent installing static libs with standalone Boost::regex

--- external/boost_regex.cmake
+++ external/boost_regex.cmake
@@ -1,4 +1,4 @@
-MESSAGE( STATUS "[Boost Regex]" )
+message(STATUS "[Boost Regex]")
 
 set(BOOST_ENABLE_CMAKE ON CACHE INTERNAL "")
 
@@ -6,8 +6,8 @@ find_package(Boost COMPONENTS regex)
 if(NOT Boost_FOUND)
     set(BRX_URL "https://github.com/boostorg/regex/archive/refs/tags/boost-1.79.0.zip")
 
-    MESSAGE( STATUS "\tBoost Regex Will be installed." )
-    MESSAGE( STATUS "\tURL: ${BRX_URL}" )
+    message(STATUS "\tBoost Regex Will be installed.")
+    message(STATUS "\tURL: ${BRX_URL}")
 
     set(BOOST_REGEX_STANDALONE ON CACHE INTERNAL "Get just regex from Boost libraries")
     FetchContent_Declare(
@@ -15,4 +15,9 @@ if(NOT Boost_FOUND)
         URL ${BRX_URL}
     )
     FetchContent_MakeAvailable(boost_regex)
+    
+    message(STATUS "WARNING: Cannot set install targets for a static library using standalone Boost regex.")
+    message(STATUS "         You will not be able to install this project.")
+    message(STATUS "         Either install Boost to your system, or build with -DBUILD_SHARED_LIBS=ON.")
+    SET(FDPAPI_WITH_INSTALL OFF CACHE BOOL "Override option" FORCE)
 endif()
