dcba726991134e57ff4ed75ed265e20458d7a53a
Ryan J Field <RyanJField@users.noreply.github.com>
2022-12-23 23:01:46 +0000

    Windows Fixes

--- external/boost_regex.cmake
+++ external/boost_regex.cmake
@@ -8,16 +8,12 @@ if(NOT Boost_FOUND)
 
     message(STATUS "\tBoost Regex Will be installed.")
     message(STATUS "\tURL: ${BRX_URL}")
-
     set(BOOST_REGEX_STANDALONE ON CACHE INTERNAL "Get just regex from Boost libraries")
+
     FetchContent_Declare(
-        boost_regex
+        BOOSTREGEX
         URL ${BRX_URL}
     )
-    FetchContent_MakeAvailable(boost_regex)
+    FetchContent_MakeAvailable(BOOSTREGEX)
     
-    message(STATUS "WARNING: Cannot set install targets for a static library using standalone Boost regex.")
-    message(STATUS "         You will not be able to install this project.")
-    message(STATUS "         Either install Boost to your system, or build with -DBUILD_SHARED_LIBS=ON.")
-    SET(FDPAPI_WITH_INSTALL OFF CACHE BOOL "Override option" FORCE)
 endif()
