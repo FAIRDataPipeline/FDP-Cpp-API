9a07ae648c54961db1b759bebf34f8fdb3fcd14f
Liam Pattinson <liampattinson@gmail.com>
2023-06-28 12:16:30 +0100

    clang-format, improved string security in C API
    Used clang-format on C API bits
    Better tests for valid strings/buffers in the C API
    Better error messages, and exits earlier when encountering errors.

--- include/fdp/fdp.h
+++ include/fdp/fdp.h
@@ -1,7 +1,6 @@
 #ifndef __FDP_C_API__
 #define __FDP_C_API__
 
-
 #ifdef __cplusplus
 
 #include "fdp.hxx"
@@ -13,10 +12,10 @@ extern "C" {
 /**
  * @brief Struct providing an interface to the pipeline.
  *
- * Defined in the implementation file, as it depends on C++ features. A pointer to this
- * struct should be passed to functions in the C API. Set up by the function fdp_init,
- * and finalised by fdp_finalise. Can also be generated from a C++ DataPipeline
- * using to_c_struct.
+ * Defined in the implementation file, as it depends on C++ features. A pointer
+ * to this struct should be passed to functions in the C API. Set up by the
+ * function fdp_init, and finalised by fdp_finalise. Can also be generated from
+ * a C++ DataPipeline using to_c_struct.
  */
 struct FdpDataPipeline;
 typedef struct FdpDataPipeline FdpDataPipeline;
@@ -24,122 +23,120 @@ typedef struct FdpDataPipeline FdpDataPipeline;
 /**
  * @brief Enumeration used to denote different error types.
  *
- * The underlying C++ API will raise a number of different exception types. These map to
- * integer error codes for C compatibility.
+ * The underlying C++ API will raise a number of different exception types.
+ * These map to integer error codes for C compatibility.
  */
 enum FdpError {
-    FDP_ERR_NONE = 0,
-    FDP_ERR_CONFIG_PARSE = 1,
-    FDP_ERR_REST_API_QUERY = 2,
-    FDP_ERR_JSON_PARSE = 3,
-    FDP_ERR_VALIDATION = 4,
-    FDP_ERR_SYNC = 5,
-    FDP_ERR_WRITE = 6,
-    FDP_ERR_TOML = 7,
-    FDP_ERR_OTHER = 8
+  FDP_ERR_NONE = 0,
+  FDP_ERR_CONFIG_PARSE = 1,
+  FDP_ERR_REST_API_QUERY = 2,
+  FDP_ERR_JSON_PARSE = 3,
+  FDP_ERR_VALIDATION = 4,
+  FDP_ERR_SYNC = 5,
+  FDP_ERR_WRITE = 6,
+  FDP_ERR_TOML = 7,
+  FDP_ERR_OTHER = 8
 };
 typedef enum FdpError FdpError;
-
 
 /**
  * @brief Initialise the pipeline.
  *
- * Should be called once before any calls to fdp_link_read or fdp_link_write. If called
- * more than once, returns FDP_ERR_OTHER.
- * 
- * @param data_pipeline     Pointer-to-pointer of a FdpDataPipeline object. The user
- *                          should declare a pointer to a FdpDataPipeline, and pass its
- *                          address to this function. This function will then initialise
- *                          the pipeline.
- * @param config_file_path  Path to the `config.yaml` file for this FDP run. Should
- *                          be at the location `${FDP_CONFIG_DIR}/config.yaml`.
- * @param script_file_path  Path to the script which initiates this FDP run. Should
- *                          be at the location `${FDP_CONFIG_DIR}/script.sh` (or
- *                          `${FDP_CONFIG_DIR}/script.bat` on Windows).
- * @param token             Token used to connect to FDP registry. May be set to `NULL`.
+ * Should be called once before any calls to fdp_link_read or fdp_link_write. If
+ * called more than once, returns FDP_ERR_OTHER.
+ *
+ * @param data_pipeline Pointer-to-pointer of a FdpDataPipeline object. The
+ * user should declare a pointer to a FdpDataPipeline, and pass its address to
+ * this function. This function will then initialise the pipeline.
+ *
+ * @param config_file_path Path to the `config.yaml` file for this FDP run.
+ * Should be at the location `${FDP_CONFIG_DIR}/config.yaml`.
+ *
+ * @param script_file_path Path to the script which initiates this FDP run.
+ * Should be at the location `${FDP_CONFIG_DIR}/script.sh` on Linux/Mac or
+ * `${FDP_CONFIG_DIR}/script.bat` on Windows.
+ *
+ * @param token Token used to connect to FDP registry. May be set to `NULL`.
  *
  * @return Error code.
  */
-FdpError fdp_init(
-    FdpDataPipeline **data_pipeline,
-    const char *config_file_path,
-    const char *script_file_path,
-    const char *token
-);
-
+FdpError fdp_init(FdpDataPipeline **data_pipeline, const char *config_file_path,
+                  const char *script_file_path, const char *token);
 
 /**
  * @brief Finalise the pipeline.
  *
  * Must be called after a call to fdp_init.
- * 
- * Record all data products and meta data to the registry. Update the code run with all
- * appropriate meta data.
  *
- * @param data_pipeline Pointer-to-pointer of a FdpDataPipeline object. This function
- *                      finalises the FdpDataPipeline, and sets its pointer to NULL.
- * 
+ * Record all data products and meta data to the registry. Update the code run
+ * with all appropriate meta data.
+ *
+ * @param data_pipeline Pointer-to-pointer of a FdpDataPipeline object. This
+ * function finalises the FdpDataPipeline, and sets its pointer to NULL.
+ *
  * @return Error code.
  */
 FdpError fdp_finalise(FdpDataPipeline **data_pipeline);
 
-
 /**
- * @brief Set a path to a given data product while recording it's meta data for the
- *        code run.
+ * @brief Set a path to a given data product while recording its meta data for
+ * the code run.
  *
  * Must be called after fdp_init and before fdp_finalise.
- * 
- * @param data_pipeline   Pointer to a FdpDataPipeline object.
- * @param data_product    Path to the input file.
- * @param data_store_path Path to the assigned data store location. The user should
- *                        allocate sufficient memory beforehand.
+ *
+ * @param data_pipeline Pointer to a FdpDataPipeline object.
+ *
+ * @param data_product Path to the input file.
+ *
+ * @param data_store_path Path to the assigned data store location. The user
+ * should allocate sufficient memory beforehand.
+ *
+ * @param data_store_path_len Size of the buffer used for the data store
+ * path.
+ *
  * @return Error code
  */
-FdpError fdp_link_read(
-    FdpDataPipeline *data_pipeline,
-    const char *data_product,
-    char *data_store_path
-);
-
+FdpError fdp_link_read(FdpDataPipeline *data_pipeline, const char *data_product,
+                       char *data_store_path, size_t data_store_path_len);
 
 /**
- * @brief Set a path to a given data product while recording it's meta data for the
- *        code run.
+ * @brief Set a path to a given data product while recording its meta data for
+ * the code run.
  *
  * Must be called after fdp_init and before fdp_finalise.
- * 
- * @param data_pipeline   Pointer to a FdpDataPipeline object.
- * @param data_product    Path to the output file.
- * @param data_store_path Path to the assigned data store location. The user should
- *                        allocate sufficient memory beforehand.
+ *
+ * @param data_pipeline Pointer to a FdpDataPipeline object.
+ *
+ * @param data_product Path to the output file.
+ *
+ * @param data_store_path Path to the assigned data store location. The user
+ * should allocate sufficient memory beforehand.
+ *
+ * @param data_store_path_len Size of the buffer used for the data store
+ * path.
+ *
  * @return Error code
  */
-FdpError fdp_link_write(
-    FdpDataPipeline *data_pipeline,
-    const char *data_product,
-    char *data_store_path
-);
-
+FdpError fdp_link_write(FdpDataPipeline *data_pipeline,
+                        const char *data_product, char *data_store_path, size_t data_store_path_len);
 
 /**
  * @brief Enumeration used to denote the different levels of logging.
  *
- * Each level of logging includes all log levels greater than it, so setting the log
- * level to `DEBUG` will include all log types except `TRACE`. These correspond to
- * the C++ logging levels `FairDataPipeline::logging::LOG_LEVEL`.
+ * Each level of logging includes all log levels greater than it, so setting the
+ * log level to `DEBUG` will include all log types except `TRACE`. These
+ * correspond to the C++ logging levels `FairDataPipeline::logging::LOG_LEVEL`.
  */
 enum FdpLogLevel {
-    FDP_LOG_TRACE = 0,
-    FDP_LOG_DEBUG = 1,
-    FDP_LOG_INFO = 2,
-    FDP_LOG_WARN = 3,
-    FDP_LOG_ERROR = 4,
-    FDP_LOG_CRITICAL = 5,
-    FDP_LOG_OFF = 6
+  FDP_LOG_TRACE = 0,
+  FDP_LOG_DEBUG = 1,
+  FDP_LOG_INFO = 2,
+  FDP_LOG_WARN = 3,
+  FDP_LOG_ERROR = 4,
+  FDP_LOG_CRITICAL = 5,
+  FDP_LOG_OFF = 6
 };
 typedef enum FdpLogLevel FdpLogLevel;
-
 
 /**
  * @brief Set the log level. Must call `fdp_init` first.
@@ -148,7 +145,6 @@ typedef enum FdpLogLevel FdpLogLevel;
  */
 void fdp_set_log_level(FdpLogLevel log_level);
 
-
 /**
  * @brief Get the current log level. Must call fdp_init first.
  *
@@ -156,19 +152,21 @@ void fdp_set_log_level(FdpLogLevel log_level);
  */
 FdpLogLevel fdp_get_log_level();
 
-
 /**
- * @brief Write a message to the log. This will be passed to the C++ logger,
- *        `FairDataPipeline::logger::get_logger()->level() << msg`, where `level`
- *        is one of `trace`, `debug`, `info`, `warn`, `error`, or `critical`.
+ * @brief Write a message to the log.
  *
- * @param log_level The type of log message to write, e.g. FDP_LOG_INFO, FDP_LOG_ERROR.
- * @param msg       The message to be written to log.
+ * This will be passed to the C++ logger,
+ * `FairDataPipeline::logger::get_logger()->level() << msg`, where
+ * `level` is one of `trace`, `debug`, `info`, `warn`, `error`, or `critical`.
+ *
+ * @param log_level The type of log message to write, e.g. FDP_LOG_INFO,
+ * FDP_LOG_ERROR.
+ *
+ * @param msg The message to be written to log.
  *
  * @return Error code. 1 if logging unsuccessful, 0 otherwise.
  */
 int fdp_log(FdpLogLevel log_level, const char *msg);
-
 
 #ifdef __cplusplus
 
@@ -184,22 +182,21 @@ DataPipeline::sptr from_c_struct(FdpDataPipeline *data_pipeline);
 /**
  * @brief Convert data pipeline from the C++ API to one in the C API.
  *
- * If the pipeline is set up using the C++ method DataPipeline::construct, this may be
- * used to generate a C-compatible struct. Note that this uses 'new' to allocate the
- * returned pointer, so the user should 'delete_c_struct` after use to avoid memory
- * leaks. It is not recommended to mix usage of the C and C++ APIs for init and finalise
- * functions.
+ * If the pipeline is set up using the C++ method DataPipeline::construct, this
+ * may be used to generate a C-compatible struct. Note that this uses 'new' to
+ * allocate the returned pointer, so the user should 'delete_c_struct` after use
+ * to avoid memory leaks. It is not recommended to mix usage of the C and C++
+ * APIs for init and finalise functions.
  */
-FdpDataPipeline* to_c_struct(DataPipeline::sptr data_pipeline);
+FdpDataPipeline *to_c_struct(DataPipeline::sptr data_pipeline);
 
 /**
  * @brief Function to clean up FdpDataPipeline created by to_c_struct
  */
 void delete_c_struct(FdpDataPipeline *data_pipeline);
 
-} // close namespace FairDataPipeline
+} // namespace FairDataPipeline
 
 #endif
-
 
 #endif // __FDP_C_API__
--- test/test_c_api.cxx
+++ test/test_c_api.cxx
@@ -3,21 +3,20 @@
 #endif
 
 #include <cstring>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 
 #include "fdp/fdp.h"
 #include "fdp/objects/metadata.hxx" // read_token
 
-#include <ghc/filesystem.hpp>
 #include "gtest/gtest.h"
+#include <ghc/filesystem.hpp>
 
 namespace fs = ghc::filesystem;
 namespace fdp = FairDataPipeline;
 
-
-std::string home_dir(){
+std::string home_dir() {
   std::string home;
 #ifdef _WIN32
   home = getenv("HOMEDRIVE");
@@ -28,31 +27,26 @@ std::string home_dir(){
   return home;
 }
 
+#define BUFFER_SIZE 512
 
-TEST(CTest, link_read_write){
+TEST(CTest, link_read_write) {
   fdp_set_log_level(FDP_LOG_DEBUG);
-  char buf[512];
+  char buf[BUFFER_SIZE];
 
   // Initialise
-  FdpDataPipeline* pipeline;
+  FdpDataPipeline *pipeline;
   fs::path config = fs::path(TESTDIR) / "data" / "write_csv.yaml";
   fs::path script = fs::path(TESTDIR) / "test_script.sh";
-  std::string token = fdp::read_token(
-    fs::path(home_dir()) / ".fair" / "registry" / "token"
-  );
-  ASSERT_EQ(
-    fdp_init(
-        &pipeline,
-        config.string().c_str(),
-        script.string().c_str(),
-        token.c_str()
-    ),
-    FDP_ERR_NONE
-  );
+  std::string token =
+      fdp::read_token(fs::path(home_dir()) / ".fair" / "registry" / "token");
+  ASSERT_EQ(fdp_init(&pipeline, config.string().c_str(),
+                     script.string().c_str(), token.c_str()),
+            FDP_ERR_NONE);
 
   // Test link write
   buf[0] = '\0'; // Ensure strlen of output buffer is 0
-  EXPECT_EQ(fdp_link_write(pipeline, "test/csv", buf), FDP_ERR_NONE);
+  EXPECT_EQ(fdp_link_write(pipeline, "test/csv", buf, BUFFER_SIZE),
+            FDP_ERR_NONE);
   EXPECT_GT(strlen(buf), 1);
 
   // Write to new path
@@ -63,45 +57,39 @@ TEST(CTest, link_read_write){
   // Finalise and re-initialise
   ASSERT_EQ(fdp_finalise(&pipeline), FDP_ERR_NONE);
   config = fs::path(TESTDIR) / "data" / "read_csv.yaml";
-  ASSERT_EQ(
-    fdp_init(
-        &pipeline,
-        config.string().c_str(),
-        script.string().c_str(),
-        token.c_str()
-    ),
-    FDP_ERR_NONE
-  );
+  ASSERT_EQ(fdp_init(&pipeline, config.string().c_str(),
+                     script.string().c_str(), token.c_str()),
+            FDP_ERR_NONE);
 
   // Test link read
   buf[0] = '\0'; // Ensure strlen of output buffer is 0
-  EXPECT_EQ(fdp_link_read(pipeline, "test/csv", buf), FDP_ERR_NONE);
+  EXPECT_EQ(fdp_link_read(pipeline, "test/csv", buf, BUFFER_SIZE),
+            FDP_ERR_NONE);
   EXPECT_GT(strlen(buf), 1);
 
   // Finalise again
   EXPECT_EQ(fdp_finalise(&pipeline), FDP_ERR_NONE);
 }
 
-TEST(CTest, cpp_to_c){
+TEST(CTest, cpp_to_c) {
   fdp_set_log_level(FDP_LOG_DEBUG);
   char buf[512];
 
   // Initialise using C++
   fs::path config = fs::path(TESTDIR) / "data" / "write_csv.yaml";
   fs::path script = fs::path(TESTDIR) / "test_script.sh";
-  std::string token = fdp::read_token(
-    fs::path(home_dir()) / ".fair" / "registry" / "token"
-  );
-  auto cpp_pipeline = fdp::DataPipeline::construct(
-    config.string(), script.string(), token
-  );
+  std::string token =
+      fdp::read_token(fs::path(home_dir()) / ".fair" / "registry" / "token");
+  auto cpp_pipeline =
+      fdp::DataPipeline::construct(config.string(), script.string(), token);
 
   // Switch to C API, use temporary FdpDataPipeline
   FdpDataPipeline *c_pipeline = fdp::to_c_struct(cpp_pipeline);
 
   // Test link write
   buf[0] = '\0'; // Ensure strlen of output buffer is 0
-  EXPECT_EQ(fdp_link_write(c_pipeline, "test/csv", buf), FDP_ERR_NONE);
+  EXPECT_EQ(fdp_link_write(c_pipeline, "test/csv", buf, BUFFER_SIZE),
+            FDP_ERR_NONE);
   EXPECT_GT(strlen(buf), 1);
 
   // Write to new path
@@ -116,25 +104,18 @@ TEST(CTest, cpp_to_c){
   cpp_pipeline->finalise();
 }
 
-TEST(CTest, c_to_cpp){
+TEST(CTest, c_to_cpp) {
   fdp_set_log_level(FDP_LOG_DEBUG);
 
   // Initialise using C
-  FdpDataPipeline* c_pipeline;
+  FdpDataPipeline *c_pipeline;
   fs::path config = fs::path(TESTDIR) / "data" / "write_csv.yaml";
   fs::path script = fs::path(TESTDIR) / "test_script.sh";
-  std::string token = fdp::read_token(
-    fs::path(home_dir()) / ".fair" / "registry" / "token"
-  );
-  ASSERT_EQ(
-    fdp_init(
-        &c_pipeline,
-        config.string().c_str(),
-        script.string().c_str(),
-        token.c_str()
-    ),
-    FDP_ERR_NONE
-  );
+  std::string token =
+      fdp::read_token(fs::path(home_dir()) / ".fair" / "registry" / "token");
+  ASSERT_EQ(fdp_init(&c_pipeline, config.string().c_str(),
+                     script.string().c_str(), token.c_str()),
+            FDP_ERR_NONE);
 
   // Switch to C++ API
   auto cpp_pipeline = fdp::from_c_struct(c_pipeline);
@@ -156,9 +137,9 @@ TEST(CTest, c_to_cpp){
   EXPECT_EQ(fdp_finalise(&c_pipeline), FDP_ERR_NONE);
 }
 
-TEST(CTest, log_levels){
-    fdp_set_log_level(FDP_LOG_INFO);
-    EXPECT_EQ(fdp_get_log_level(), FDP_LOG_INFO);
-    fdp_set_log_level(FDP_LOG_DEBUG);
-    EXPECT_EQ(fdp_get_log_level(), FDP_LOG_DEBUG);
+TEST(CTest, log_levels) {
+  fdp_set_log_level(FDP_LOG_INFO);
+  EXPECT_EQ(fdp_get_log_level(), FDP_LOG_INFO);
+  fdp_set_log_level(FDP_LOG_DEBUG);
+  EXPECT_EQ(fdp_get_log_level(), FDP_LOG_DEBUG);
 }
